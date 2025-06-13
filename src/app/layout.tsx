05ddb8d99085412d1eb2a33d90990d85b5acab6f
akira1ce <akira1ce@163.com>
2025-06-13 19:18:25 +0800

    feat: add custom MapleMono font and update prose styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,11 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Header from './components/header';
 import { ThemeProvider } from '../components/theme-provider';
+import localFont from 'next/font/local';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+const mapleMono = localFont({
+  src: './MapleMono-Regular.woff2',
 });
 
 export const metadata: Metadata = {
@@ -26,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} bg-main min-h-screen w-screen antialiased`}
-      >
+      <body className={`${mapleMono.className} bg-main min-h-screen w-screen antialiased`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
