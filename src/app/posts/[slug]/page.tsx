27c916339801e4fcbeeb1956ef9bb48f0dd135a2
akira1ce <akira1ce@163.com>
2025-08-11 11:06:31 +0800

    fix: 更新文章页面页脚内容，调整标签样式以改善用户体验

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -59,7 +59,7 @@ export default async function Page({ params }: Props) {
           maxLevel={4}
         />
         <footer className="border-fore/10 text-fore/60 mt-20 gap-2 border-t pt-10 text-center">
-          is done, by akira1ce.
+          「 Is All 」
         </footer>
       </div>
     </>
