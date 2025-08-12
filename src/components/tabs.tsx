a6b4071a5f07a2e68f646f85daf0080760f548a1
akira1ce <akira1ce@163.com>
2025-08-12 10:31:26 +0800

    feat: 重构 Tabs 组件，使用索引管理活动标签，优化渲染逻辑和样式

--- src/components/tabs.tsx
+++ src/components/tabs.tsx
@@ -1,12 +1,11 @@
 'use client';
 
 import { cn } from '@/lib/utils';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useId, useState } from 'react';
 import { motion } from 'motion/react';
 import { FadeInUp } from './fade-in-up';
 
 export interface TabItemProps {
-  label: string;
   children: React.ReactNode;
 }
 
@@ -19,18 +18,20 @@ export interface TabsProps {
 export const Tabs = ({ children }: TabsProps) => {
   const tabs = Array.isArray(children) ? children : [children];
 
-  const [activeTab, setActiveTab] = useState(() => tabs[0]?.props?.label || '');
+  const [activeTab, setActiveTab] = useState(0);
+
+  const uid = useId();
 
   return (
     <div>
       <div className="flex items-center gap-2">
-        {tabs.map((child: any) => {
+        {tabs.map((child: any, index) => {
           const label = child.props.label;
-          const isActive = activeTab === label;
+          const isActive = activeTab === index;
           return (
             <div
-              key={label}
-              onClick={() => setActiveTab(label)}
+              key={`${uid}-${index}`}
+              onClick={() => setActiveTab(index)}
               className={cn(
                 'relative cursor-pointer rounded-xl px-2 py-1 transition-all',
                 isActive ? 'font-bold' : 'hover:bg-fore/10 font-normal',
@@ -40,7 +41,7 @@ export const Tabs = ({ children }: TabsProps) => {
               {isActive && (
                 <motion.div
                   className="bg-fore/10 absolute bottom-0 left-0 h-full w-full rounded-xl"
-                  layoutId="tab-active"
+                  layoutId={`${uid}-tab-active`}
                   transition={{ type: 'spring', stiffness: 300, damping: 30 }}
                 />
               )}
@@ -49,11 +50,7 @@ export const Tabs = ({ children }: TabsProps) => {
         })}
       </div>
       <div className="-mt-4">
-        {tabs.map((child: any) =>
-          child.props.label === activeTab ? (
-            <FadeInUp key={child.props.label}>{child.props.children}</FadeInUp>
-          ) : null,
-        )}
+        <FadeInUp key={`${uid}-${activeTab}`}>{tabs[activeTab]?.props.children}</FadeInUp>
       </div>
     </div>
   );
