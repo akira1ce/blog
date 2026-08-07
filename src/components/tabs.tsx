ae2e6c13287d5a6cd7d8b41f7f299e7b13781695
akira1ce <akira1ce@163.com>
2026-08-07 18:05:41 +0800

    refactor(tabs): improve tab item structure and styling

--- src/components/tabs.tsx
+++ src/components/tabs.tsx
@@ -6,6 +6,7 @@ import { motion } from 'motion/react';
 import { FadeInUp } from './fade-in-up';
 
 export interface TabItemProps {
+  label: React.ReactNode;
   children: React.ReactNode;
 }
 
@@ -23,32 +24,31 @@ export const Tabs = ({ children }: TabsProps) => {
   const uid = useId();
 
   return (
-    <div className="border-border-color tabs-wrapper my-4 rounded-xl border p-2">
-      <div className="mb-2 flex items-center gap-2 overflow-auto">
+    <div className="tabs-wrapper my-4">
+      <div className="bg-accent mb-4 flex w-fit max-w-full items-center gap-2 overflow-auto rounded-md border border-dashed p-1">
         {tabs.map((child: any, index) => {
           const label = child.props.label;
           const isActive = activeTab === index;
+
           return (
             <div
               key={`${uid}-${index}`}
               onClick={() => setActiveTab(index)}
-              className={cn(
-                'relative cursor-pointer rounded-xl px-2 py-1 text-nowrap transition-colors',
-                isActive ? 'text-accent-sky' : 'text-fore/60 hover:text-fore',
-              )}
+              className="relative cursor-pointer rounded-xl px-2 py-1 text-nowrap"
             >
-              {label}
               {isActive && (
                 <motion.div
-                  className="bg-accent-sky/10 absolute bottom-0 left-0 h-full w-full rounded-xl"
+                  className="bg-main/90 absolute inset-0 z-0 rounded-md"
                   layoutId={`${uid}-tab-active`}
                   transition={{ type: 'spring', stiffness: 300, damping: 30 }}
                 />
               )}
+              <span className={'relative z-10 transition-colors'}>{label}</span>
             </div>
           );
         })}
       </div>
+
       <div>
         <FadeInUp key={activeTab} duration={0.1}>
           {tabs[activeTab]?.props.children}
